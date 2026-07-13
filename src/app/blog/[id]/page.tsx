6e2e9167232baed862707129abbe01c2da856c42
yzg011 <357239268@qq.com>
2026-07-13 16:38:26 +0800

    Delete src/app/blog/[id]/page.tsx

--- src/app/blog/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-export const runtime = "edge";
-import BlogClient from "./BlogClient";
-
-type Params = { id: string };
-type Props = { params: Params };
-
-export default function BlogDetailPage({ params }: Props) {
-  return <BlogClient slug={params.id} />;
-}
